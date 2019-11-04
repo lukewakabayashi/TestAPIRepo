879167f4d34aee817183c9307c0061c8fa8b282d
Luke <lukewakabayashi@Frazer.biz>
2019-11-04 11:46:26 -0500

    test

--- main.go
+++ main.go
@@ -3,9 +3,5 @@ package main
 import "fmt"
 
 func main() {
-<<<<<<< HEAD
-	fmt.Println("Testing....")
-=======
-	fmt.Println("Test")
->>>>>>> f12dda79126d4c0c226881bde9696895d5d0f659
+	fmt.Println("Testing")
 }
