867b4ae913ecb84aa3888626f135e135e5fb39ce
Luke <lukewakabayashi@Frazer.biz>
2019-11-19 13:40:44 -0500

    I

--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/frazercomputing/owner-verification-portal-back-end/auth"
+)
 
 func main() {
-	fmt.Println("Testing")
+	fmt.Println(auth.NewDefault().Sign(18292))
+	fmt.Println(auth.New("2347").Sign(18292))
 }
